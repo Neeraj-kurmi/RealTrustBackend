95757ba7408ce6141545bab7417f370d60047c03
Neeraj-kurmi <neerajkurmi2002@gmail.com>
2025-07-10 19:26:57 +0530

    index file changed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-  origin: "https://real-trust-seven.vercel.app/",
+  origin: "https://real-trust-one.vercel.app/",
   credentials: true
 }));
 app.use(express.json({ limit: '5mb' }));
